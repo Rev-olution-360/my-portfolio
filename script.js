0e55f5d7f24150f1b1761dd5b628761260e19c2b
Rev-olution-360 <iamvictory0257200@gmail.com>
2024-11-07 10:24:08 -0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function navigateToPortfolio() {
+    document.getElementById('intro').style.display='none';
+    document.getElementById('portfolio').style.display='block';
+}
+
+function navigateToIntro() {
+    document.getElementById('portfolio').style.display='none';
+    document.getElementById('intro').style.display='block';
+}
